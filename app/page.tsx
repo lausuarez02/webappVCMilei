86b1b18e013b6eebc4fd3cc16e379e63989de8c1
Lautaro <lausuarez71@gmail.com>
2025-01-22 09:45:20 -0300

    Add: working page and hide dashboard for now

--- app/page.tsx
+++ app/page.tsx
@@ -22,14 +22,6 @@ export default function LandingPage() {
       <header className="w-full p-4 bg-black">
         <div className="flex justify-center items-center max-w-7xl mx-auto">
           <div className="flex items-center gap-2">
-            {/* <Image 
-              src="/logo.png" 
-              alt="VCMilei" 
-              width={140} 
-              height={140}
-              className="object-contain"
-            /> */}
-            {/* <h1 className="text-2xl font-bold text-violet-400">VCMilei</h1> */}
           </div>
         </div>
       </header>
@@ -52,13 +44,14 @@ export default function LandingPage() {
           <div className="flex flex-col md:flex-row justify-center gap-6">
             <Button 
               className="bg-gradient-to-r from-blue-500 to-violet-400 hover:from-blue-600 hover:to-violet-500 text-white px-12 py-8 text-xl font-bold rounded-xl transform hover:scale-105 transition-all w-full md:w-auto"
-              onClick={() => window.location.href = '/dashboard'}
-            >
+              // onClick={() => window.location.href = '/dashboard'}
+              onClick={() => window.location.href = '/working'}
+              >
               Go to Dashboard
             </Button>
             <Button 
               className="bg-zinc-800 hover:bg-zinc-700 text-violet-300 border border-violet-300/20 px-12 py-8 text-xl font-bold rounded-xl transform hover:scale-105 transition-all w-full md:w-auto"
-              onClick={() => window.location.href = '/invest'}
+              onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSd5oW17JZ0JD65PbwRRtQrQ7OVG4qr1O1qvqWfIoawum-zJsg/viewform?usp=dialog', '_blank')}
             >
               Pitch Me, Leftie
             </Button>
